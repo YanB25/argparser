38137b50c0050ec7bf2db44ad77f22da88d940b6
yanbin <yanb25@mail2.sysu.edu.cn>
2020-12-11 01:36:38 +0800

    pass testcase for big int

--- bin/argtest.cpp
+++ bin/argtest.cpp
@@ -1,6 +1,7 @@
-#include "gtest/gtest.h"
-#include "argparser/all.hpp"
 #include <inttypes.h>
+
+#include "argparser/all.hpp"
+#include "gtest/gtest.h"
 
 TEST(FactorialTest, HandlesZeroInput)
 {
@@ -23,7 +24,15 @@ TEST(ArgparserFlag, ParseInt)
     EXPECT_TRUE(parser->flag(&zero, "--zero", "-z", "== 0"));
     EXPECT_TRUE(parser->flag(&pos, "--positive", "-p", "> 0"));
     EXPECT_TRUE(parser->flag(&neg, "--negative", "-n", "< 0"));
-    const char* arg[] = {"./argtest", "--int", "5", "--zero", "0", "--positive", "8", "--negative", "-21"};
+    const char *arg[] = {"./argtest",
+                         "--int",
+                         "5",
+                         "--zero",
+                         "0",
+                         "--positive",
+                         "8",
+                         "--negative",
+                         "-21"};
     EXPECT_TRUE(parser->parse(sizeof(arg) / sizeof(arg[0]), arg));
     EXPECT_EQ(i, 5);
     EXPECT_EQ(zero, 0);
@@ -31,12 +40,46 @@ TEST(ArgparserFlag, ParseInt)
     EXPECT_EQ(neg, -21);
 }
 
+TEST(ArgparserFlag, ParseBigInt)
+{
+    int64_t big_int;
+    int64_t big_int_p_1;
+    int64_t big_int_m_1;
+
+    // a negative number -2^{63}
+    const int64_t expect = 1ull << 63;
+    // expect + 1
+    int64_t expect_p1 = (1ull << 63);
+    expect_p1 += 1;
+    // expect 3 1
+    int64_t expect_m1 = (1ull << 63);
+    expect_m1 += 3;
+
+    auto parser = argparser::new_parser("parse int");
+    EXPECT_TRUE(parser->flag(&big_int, "--big-int", "-b", "big number"));
+    EXPECT_TRUE(
+        parser->flag(&big_int_p_1, "--big-int-p-1", "-p", "big number + 1"));
+    EXPECT_TRUE(
+        parser->flag(&big_int_m_1, "--big-int-m-1", "-m", "big number - 1"));
+    const char *arg[] = {"./argtest",
+                         "--big-int",
+                         "-9223372036854775808",
+                         "--big-int-p-1",
+                         "-9223372036854775807",
+                         "--big-int-m-1",
+                         "-9223372036854775805"};
+    EXPECT_TRUE(parser->parse(sizeof(arg) / sizeof(arg[0]), arg));
+    EXPECT_EQ(big_int, expect);
+    EXPECT_EQ(big_int_p_1, expect_p1);
+    EXPECT_EQ(big_int_m_1, expect_m1);
+}
+
 TEST(ArgparserFlag, ParseInt64)
 {
     int64_t i = 0;
     auto parser = argparser::new_parser("parse int");
     EXPECT_TRUE(parser->flag(&i, "--int", "-i", "number i"));
-    const char* arg[] = {"./argtest", "--int", "5"};
+    const char *arg[] = {"./argtest", "--int", "5"};
     EXPECT_TRUE(parser->parse(3, arg));
     EXPECT_EQ(i, 5);
 }
